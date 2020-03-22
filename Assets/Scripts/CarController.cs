5875db52c75c3ab3ab9c1fb4367d7c59c3bcf5d8
tomoya-takubo <tkb1003@gmail.com>
2020-03-23 03:42:36 +0900

    20200323 コミット（２回目）
    オブジェクト配置～車とりあえずスワイプで走行実装

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private float speed = 0;
+    Vector2 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //スワイプの長さを決める
+        if (Input.GetMouseButtonDown(0))
+        {
+            //マウスをクリックした座標
+            this.startPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            //マウスを離した座標
+            Vector2 endPos = Input.mousePosition;
+            //終点と始点の距離
+            float swipeLength = endPos.x - startPos.x;
+
+            //スワイプの長さを初速度に変換する
+            this.speed = swipeLength / 500.0f;
+        }
+
+        transform.Translate(this.speed, 0, 0);
+        this.speed *= 0.98f;
+    }
+}
