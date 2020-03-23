c1b405bb35ae821369d7a12380eba5e8e29d8ac1
tomoya-takubo <tkb1003@gmail.com>
2020-03-23 11:43:59 +0900

    20200323 コミット（３回目）
    とりあえず完成（UI表示、車SE）

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -31,6 +31,9 @@ public class CarController : MonoBehaviour
 
             //スワイプの長さを初速度に変換する
             this.speed = swipeLength / 500.0f;
+
+            //効果音再生
+            GetComponent<AudioSource>().Play();
         }
 
         transform.Translate(this.speed, 0, 0);
